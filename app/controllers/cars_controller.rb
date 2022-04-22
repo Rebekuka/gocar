ed912d9689498bd455335ff2c923834f4da3c86b
rebecca biberian <rebeccabiberian@gmail.com>
2022-04-22 18:26:25 +0200

    enfin

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,33 +5,34 @@ class CarsController < ApplicationController
 
   def show
     @car = Car.find(params[:id])
+    @booking = Booking.new
   end
 
-  def new
-    @car = Car.new
-  end
+  # def new
+  #   @car = Car.new
+  # end
 
-  def create
-    Car.create(car_params)
-    redirect_to cars_path
-  end
+  # def create
+  #   Car.create(car_params)
+  #   # redirect_to cars_path
+  # end
 
-  def edit
-    @car = Car.find(params[:id])
-  end
+  # def edit
+  #   @car = Car.find(params[:id])
+  # end
 
-  def update
-    @car = Car.find(params[:id])
-    @car.update(car_params)
-    redirect_to cars_path
-  end
+  # def update
+  #   @car = Car.find(params[:id])
+  #   @car.update(car_params)
+  #   redirect_to cars_path
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
   private
 
   def car_params
-    params.require(:car).(:brand, :price)
+    params.require(:car).permit(:brand, :price)
   end
 end
