d6de1b9420d560cb642466d1bf392a13230cde10
rebecca biberian <rebeccabiberian@gmail.com>
2022-04-23 11:13:59 +0200

    navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,43 +1,42 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
 
-    def index
-      @bookings = Booking.all
-    end
+  def new
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new
+  end
 
-    def new
-      @car = Car.find(params[:car_id])
-      @booking = Booking.new
-    end
+  def create
+    @booking = Booking.new(booking_params)
+    @car = Car.find(params[:car_id])
+    @booking.car = @car
+    @booking.user = current_user
 
-    def create
-      @booking = Booking.new(booking_params)
-      @car = Car.find(params[:car_id])
-      @booking.car = @car
-      @booking.user = current_user
-
-      if @booking.save
-        redirect_to root_path
-      else
-        render :new
-      end
-    end
-
-    def destroy
-      @booking.destroy
-      redirect_to root_path(@booking.car)
-    end
-
-    private
-
-    def booking_params
-      params.require(:booking).permit(:duration, :user_id, :car_id)
-    end
-
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
-
-    def set_car
-      @car = Car.find(params[:car_id])
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
     end
   end
+
+  def destroy
+    @booking.destroy
+    redirect_to root_path(@booking.car)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:duration, :user_id, :car_id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+end
